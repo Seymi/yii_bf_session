c9d994112b3f3928d304de5c9cfade1d09ff8241
Martin Chaim Seymann <martin.seymann@chello.at>
2014-01-02 07:06:57 +0100

    force markets
    replaced

--- protected/views/betfair/markets.php
+++ protected/views/betfair/markets.php
@@ -1,35 +1,24 @@
 <?php
-/* @var $this BetfairController */
+/* @var $this BetfairController 
+   $events 
+*/
 
 $this->breadcrumbs = array(
     'Betfair' => array('/betfair'),
     'Markets',
 );
 ?>
+
 <h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <tt><?php echo __FILE__; ?></tt>.
-</p>
-
-
-<p><?php echo "your session: $session"; ?></p>
 
 <?php
-$ap_param = array(
-    'request' =>
-    array(
-        'session_token' => $session,
-        'clientStamp' => 0,
-    ),
-);
+
+print_r ($markets);
+print '<br/>';
 
 
-try {
-    $wsdl_url = 'https://api.betfair.com/global/v3/BFGlobalService.wsdl';
-    $client = new SoapClient($wsdl_url);
-
+<<<<<<< HEAD
     $params = array(
         'request' =>     array(
                        'header' => array(
@@ -79,3 +68,6 @@ $params = array(
 }
 
 ?>
+=======
+?>
+>>>>>>> events and markets
